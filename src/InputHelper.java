55c06b70baf64e0a276114f70c7bb45f94fe7110
Noah <scott.613865@swl.k12.oh.us>
2023-11-17 09:46:25 -0500

    g

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,5 +1,51 @@
-public class Main {
+import java.util.Scanner;
+
+public class InputHelper {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Test Methods
+        Scanner scan = new Scanner(System.in);
+        System.out.println(getInt(scan, "Please enter an int value."));
+        System.out.println(num);
+    }
+
+    //This Method Loops until a valid integer input is received. Returns int value.
+    public static int getInt(Scanner in, String prompt){
+        boolean done = false;
+        int x = 0;
+
+        System.out.println(prompt);
+        do {
+            if(in.hasNextInt()){
+                x = in.nextInt();
+                done = true;
+            }else{
+                System.out.println("Invalid input. Do better.");
+            }
+            in.nextLine(); //clear buffer
+        }while(!done);
+        return x;
+    }
+}
+
+    public static int getRangedInt(Scanner in, String prompt, int min, int max){
+        boolean done = false;
+        int x = 0;
+
+        System.out.println(prompt);
+        do {
+            if(in.hasNextInt()){
+                x = in.nextInt();
+                //check to see if value is in range.
+                if(x <= max && x >= min){
+                    done = true;
+                }else{
+                    System.out.println("Invalid input. Do better.");
+                }
+            }else{
+                System.out.println("Invalid input. Do better.");
+            }
+            in.nextLine(); //clear buffer
+        }while(!done);
+        return x;
     }
 }
