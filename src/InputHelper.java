aee41432d75daf1b3ee4d852e5884d7e9e4077fc
Noah <scott.613865@swl.k12.oh.us>
2023-11-29 09:41:36 -0500

    g

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -5,12 +5,7 @@ public class InputHelper {
         //Test Methods
         Scanner scan = new Scanner(System.in);
 
-
-
-
-        System.out.println(getYNConfirm(scan, "Please enter \"Y\" or \"N\"."));
-
-
+        System.out.println(getPositiveNonZeroInt(scan, "enter INt"));
     }
 
     //This Method Loops until a valid integer input is received. Returns int value.
@@ -120,6 +115,69 @@ public class InputHelper {
         }else if(yn == false){
             input = "false";
         }
+
+        return input;
+    }
+
+    public static double getDouble(Scanner in, String prompt){
+        double input = 0;
+        boolean valid = false;
+
+        System.out.println(prompt);
+
+        do{
+            if(in.hasNextDouble()){
+                input = in.nextDouble();
+                valid = true;
+            }else{
+                System.out.println("Yo input wrong.");
+            }
+            in.nextLine();
+        }while(!valid);
+
+        return input;
+    }
+
+    public static double getRangedDouble(Scanner in, String prompt, double min, double max){
+        boolean done = false;
+        double x = 0;
+
+        System.out.println(prompt);
+        do {
+            if(in.hasNextDouble()){
+                x = in.nextDouble();
+                //check to see if value is in range.
+                if(x <= max && x >= min){
+                    done = true;
+                }else{
+                    System.out.println("Invalid input. Do better.");
+                }
+            }else{
+                System.out.println("Invalid input. Do better.");
+            }
+            in.nextLine(); //clear buffer
+        }while(!done);
+        return x;
+    }
+
+    public static int getPositiveNonZeroInt(Scanner in, String prompt){
+        int input = 0;
+        boolean valid = false;
+
+        System.out.println(prompt);
+        do {
+            if(in.hasNextInt()){
+             input = in.nextInt();
+             if(input > 0){
+                 valid = true;
+             }else{
+                 System.out.println("need number over 0.");
+             }
+            }else{
+                System.out.println("Your bad do it again.");
+            }
+            in.nextLine();
+        }while(!valid);
 
         return input;
     }
